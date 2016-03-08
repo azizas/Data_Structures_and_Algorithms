5c936f62acca3ce06d6292004177da95e6e6da64
Abdulaziz Asiri <azizasiri90@gmail.com>
2016-03-08 15:57:07 -0800

    Added reverse function that prints recursively

--- linked_list.c
+++ linked_list.c
@@ -4,132 +4,112 @@ struct node
 {
 int data ; 
 struct node *next ; 
-
 }; 
-struct node * insert (struct node *head , int data , int position )
+void print_rec(struct node *head)
 {
-struct node *temp = malloc (sizeof(struct node )); 
-temp->data = data ; 
-temp->next= NULL ; // if position == 1
-if (position == 1)
+struct node *h = head ;
+
+while (h != NULL )
 {
-	temp->next = head ; 
-head = temp ;  // if position == 1
-}
-else 
-{
-	struct node * temp2 ; 
-	temp2 = head ; 
-	for (int i = 0 ; i<position-2 ; i++)
-  temp2 = temp2->next ; 
-  temp->next = temp2->next ; 
-  temp2->next = temp ; 
- 
+printf("%d",h->data); 
+	h = h->next ; 
+
 
 }
- return head ; 
-}
-void print (struct node *head)
-{
-	struct node *temp = head ; 
-	while (temp != NULL )
-	{
-		printf("%d,", temp->data); 
-		temp = temp->next ; 
-	}
-printf("\n"); 
 
 }
-struct node * delete (struct node * head, int del)
+struct node * pri_rec (struct node *head)
 {
 struct node *temp = head ; 
-if (del == 1)
+
+if (temp->next== NULL)
 {
-	head = temp->next ; 
-	free(temp); 
+head = temp ; 
+
+
 }
 else 
 {
-for (int i = 0; i <del-2  ;i++)
-	temp = temp->next ; 
-struct node *temp2 = temp->next ; 
-temp->next = temp2->next  ;
-free(temp2); 
-
+pri_rec(temp->next); //moves to the next node 
+struct node *q  = temp->next ; 
+q->next = temp ; 
+temp->next = NULL ; 
 }
 return head ; 
 
 }
-struct node * reverse (struct node *head)
+struct  node * insert (struct node *head ,int data )
 {
-struct node *temp , *prev , *next  ; 
-temp = head ; 
-prev = NULL ; 
-while (temp != NULL)
-{
+struct node *temp ; 
+temp = malloc(sizeof(struct node)); 
+temp->data = data ; 
+temp->next = NULL ; 
+printf("%d",temp->data);
 
-	next = temp->next ; 
-	temp->next = prev ; 
-	prev = temp ; 
-	temp = next ; 
-}
-head = prev ; 
-return head ; 
-}
-void  reverse_recuresevly (struct node * head)
-{
 if (head == NULL)
 {
+	head = temp ; 
 
-	return ; 
 }
-reverse_recuresevly(head->next); 
-printf("%d,",head->data); 
+else 
+{
+
+	struct node *temp2  = head ; 
+	while(temp2->next != NULL)
+		temp2 = temp2->next ; 
+	temp2->next = temp ; 
+}
+return head ; 
+}
+void print(struct node *head)
+
+{
+struct node *temp1 = head ; 
+while (temp1!= NULL)
+{
+	printf("%d, ",temp1->data);
+temp1= temp1->next ; 
+}
+printf("\n");
 
 }
 int main ()
-
 {
-	bool cck = true ; 
-	int choose  , data , position;	int dele;  
-	struct node *head = NULL ; 
-	
+//first insert at the end   ; 
+	struct node *head = NULL; 
+  int choose  ; 
 
   do {
-  	printf("---------------------------------\n");
-	printf ("Enter one of the following \n");
-	printf("---------------------------------\n");
-	printf("#1 insert the to the List \n");
-	printf("#2 Print The List \n"); 
-	
-	printf("#3 Delete an Element from The List \n"); 
-	printf("#4 Reverse order of List \n");
-	printf("#5  reverse_recuresevly order of List \n\n:"); 
-	scanf("%d",&choose); 
-  	switch (choose)
-  	{
-  		case 1: 
-  		printf("Enter Data to Store in the List \n"); 
-  		scanf("%d", &data); 
-  		printf("Enter a postion \n"); 
-  		scanf("%d",&position); 
-    head  =insert(head,data, position); 
-  		break ; 
-  		case 2: 
-  		print(head);
-  		break ; 
-  		case 3:
-  		printf("Enter a positoin to delete \n"); 
-  		scanf("%d",&dele); 
-  		head = delete (head , dele); 
-  		break ; 
-  		case 4: 
-  		head = reverse(head); 
-  		break; 
-  		case 5 : 
-  		reverse_recuresevly(head); 
-  	}
+  printf(" Enter one of the  following \n"); 
+  
+   printf("#1 Insert an Element in the list \n"); 
+   printf("#2 Print the List \n"); 
+   printf("#3 preverse the List \n");
+   printf("#4 print_recuresivly  elements in the list \n\n: "); 
+  scanf("%d",&choose); 
+   switch (choose)
+   {
 
-  }while (choose >=1 && choose <=5); 
-printf ("Sorry, Beyond Boundries \n "); 
+case 1: 
+printf (" Enter an Element \n"); 
+int data  ; 
+scanf("%d",&data); 
+head = insert(head , data); 
+break ; 
+case 2: 
+print(head); 
+break; 
+case 3: 
+ head=pri_rec(head); 
+break ; 
+case 4: 
+ print_rec(head); 
+break ; 
+
+
+   }
+
+  }while  (choose>=1 && choose <=4); 
+  printf (" Beyond Boundries  \n"); 
+
 }
