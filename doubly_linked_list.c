ccf8b34df6d3ef0ee451ebe35230bfb6c4b3a9c2
Abdulaziz Asiri <azizasiri90@gmail.com>
2016-03-08 20:37:57 -0800

    two more operations
    insert at tail,  delete.

--- doubly_linked_list.c
+++ doubly_linked_list.c
@@ -8,7 +8,17 @@ struct node *next ;
 struct node *pre ; 
 
 }; 
-struct node * insertAt_Beg`inning (struct node *head , int data )
+void print_test (struct node *head)
+{
+struct node * n = head ; 
+while (n->next!= NULL)
+{
+	n = n->next ; 
+}
+printf("Last node Value = %d\n",n->data); 
+
+}
+struct node * insertAt_Beginning (struct node *head , int data )
 {
 struct node *temp = malloc (sizeof (struct node)); 
 temp->data = data ; 
@@ -28,6 +38,35 @@ else
 }
 return head ; 
 }
+struct node * delete (struct node * head , int del)
+{
+struct node *temp ; 
+temp = head ; 
+if(del==1)
+{
+head = temp->next  ;
+temp->pre =NULL; 
+free (temp); 
+}
+else
+{
+
+
+	for (int i = 0 ; i<del-2; i++)
+	temp = temp->next; 
+
+	struct node *temp2 ; 
+	temp2= temp->next; 
+	temp->next = temp2->next ; 
+	temp2->pre = temp;
+	free(temp2); 
+}
+
+
+
+return head; 
+
+}
 void print(struct node *head)
 {
 struct node *temp = head ; 
@@ -43,7 +82,7 @@ void print_reveersely(struct node *head)
 {
 	printf("Print reversly \n\n"); 
 struct node *temp =head; 
-while (temp->next != NULL )
+while (temp->next != NULL )// differece between temp->next , temp = NULL
 temp = temp->next ;  
 while (temp != NULL)
 {
@@ -53,33 +92,83 @@ while (temp != NULL)
  }  
  printf("\n");    
 }
-void delete ()
+struct node * insertAt_tail (struct node *head , int data )
 {
+struct node *temp = malloc(sizeof(struct node));
+struct node *str2 ; 
+str2 = head ;  
+temp->data = data ; 
+temp->pre= NULL ;
+ temp->next = NULL ; 
+ while (str2->next != NULL)
+ 	str2 = str2->next ; 
+ str2->next = temp  ; 
+ temp->pre= str2 ; 
 
+  return head ; 
 
 }
-void insertAt_Tail ()
-{
 
 
-
-}
 int main ()
 {
- 
+ int ch ;int i ; 
 struct node *head= NULL ; 
+	int add ,dele;
 
-//cin>>value; 
-head = insertAt_Beginning(head, 11); 
-head = insertAt_Beginning(head, 12); 
-head = insertAt_Beginning(head, 13); 
-head = insertAt_Beginning(head, 14); 
+
+
+do {
+
+
+
+printf (" Add in the beginning = 1  Add in the Tail = 2  Delete = 3  print = 4  Run Test = 5 Print_reversly = 6\n\n"); 
+scanf("%d",&ch);
+switch (ch)
+{
+
+
+
+
+case 1: 
+
+
+
+	printf(" Enter a value \n"); 
+	scanf("%d",&add); 
+
+	head= insertAt_Beginning(head,add); 
+ 
+break ;
+case 2: 
+
+printf(" Enter a value \n"); 
+	scanf("%d",&i); 
+	head = insertAt_tail(head,i); 
+break ; 
+	case 3: 
+
+printf(" Enter Node To delete \n");
+
+scanf("%d",&dele); 
+head= delete(head, dele);
+break ; 
+
+case 4: 
 
 print(head); 
+
+break ;
+
+case 5: 
+print_test(head); 
+break ; 
+case 6: 
 print_reveersely(head); 
-
-
-
-
+break ; 
+}
+}
+while (ch>=1 && ch<=6);
+printf(" Beyond Boundries \n"); 
 
 }
